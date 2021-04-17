935769fb693a02e2b254fa5e542d27f9f0a3d543
pierluigi <pierluigibaiano@gmail.com>
2021-04-17 18:41:30 +0200

    display data from JSON

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ function App() {
   return (
     <div className="App">
       <h1>Song Database</h1>
-      <p>There are {songs.length} songs</p>
+      <p>There are {songs.length} songs.</p>
+      <ul>
+        {songs.map((song) => {
+          return (
+            <>
+              <li>
+                {song.author} - {song.name}
+              </li>
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 }
