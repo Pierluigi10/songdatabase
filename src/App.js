ee0d6719a332289d3b9e9ee0a5c324fa05d239f4
pierluigi <pierluigibaiano@gmail.com>
2021-04-17 18:23:13 +0200

    import fie JASON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import songs from "./data/songs.json";
 
 function App() {
   return (
     <div className="App">
       <h1>Song Database</h1>
-      Test
+      <p>There are {songs.length} songs</p>
     </div>
   );
 }
