f689da649e63c86ebe1295d1898406d32543ad2b
pierluigi <pierluigibaiano@gmail.com>
2021-04-17 19:15:22 +0200

    make static search work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import "./App.css";
 import songs from "./data/songs.json";
 
+const searchText = "HAND";
+
 function App() {
-  const searchSongs = (searchText) =>{
+  const searchSongs = (searchText) => {
     console.log(searchText);
-  }
+  };
   return (
     <div className="App">
       <h1>Song Database</h1>
       <div>
-        <input type="text" onChange={((e) => searchSongs(e.target.value))}/>
+        <input type="text" onChange={(e) => searchSongs(e.target.value)} />
       </div>
       <p>There are {songs.length} songs.</p>
       <ul>
         {songs.map((song) => {
           return (
             <>
-              <li>
-                {song.author} - {song.name}
-              </li>
+              {(song.author.toLowerCase().includes(searchText.toLowerCase()) ||
+                song.name.toLowerCase().includes(searchText.toLowerCase())) && (
+                  <li>
+                    {song.author} - {song.name}
+                  </li>
+                )}
             </>
           );
         })}
