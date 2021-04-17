3e17eb547ba1972130d966d60d2d6687bd3073de
pierluigi <pierluigibaiano@gmail.com>
2021-04-17 18:59:20 +0200

    send data to searchSongs function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import "./App.css";
 import songs from "./data/songs.json";
 
 function App() {
+  const searchSongs = (searchText) =>{
+    console.log(searchText);
+  }
   return (
     <div className="App">
       <h1>Song Database</h1>
+      <div>
+        <input type="text" onChange={((e) => searchSongs(e.target.value))}/>
+      </div>
       <p>There are {songs.length} songs.</p>
       <ul>
         {songs.map((song) => {
