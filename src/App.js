b5737bb600247ea45b03080ce51732f79d723970
pierluigi <pierluigibaiano@gmail.com>
2021-04-18 19:30:12 +0200

    finish interaction

--- src/App.js
+++ src/App.js
@@ -4,40 +4,41 @@ import initialiSongs from "./data/songs.json";
 
 function App() {
   const [searchText, setSearchtext] = useState("");
-  const [songs, setSongs] = useState(initialiSongs)
+  const [songs, setSongs] = useState(initialiSongs);
 
   const searchSongs = (userSearchText) => {
     setSearchtext(userSearchText);
   };
 
   const toggleDescription = (index) => {
-   songs[index].showDescription = !songs[index].showDescription;
-    console.log(songs[index]);
-    console.log(`You ckicked song with index ${index}`)
+    songs[index].showDescription = !songs[index].showDescription;
     setSongs([...songs]);
-    
-  }
+  };
   return (
     <div className="App">
       <h1>Song Database</h1>
       <div>
-        <input type="text" onChange={((e) => searchSongs(e.target.value))} />
+        <input type="text" onChange={(e) => searchSongs(e.target.value)} />
       </div>
-      <p>There are {songs.length} songs.</p>
+      <p>
+        There are {songs.length} songs and{" "}
+        {songs.filter((song) => song.showDescription).length} are showing.
+      </p>
       <ul>
         {songs.map((song, index) => {
-
           return (
             <>
               {(song.author.toLowerCase().includes(searchText.toLowerCase()) ||
                 song.name.toLowerCase().includes(searchText.toLowerCase())) && (
-                <li>{index}. <span
-                  onClick={() => toggleDescription(index)}>{song.author} - {song.name}</span>
-
+                <li>
+                  {index}.{" "}
+                  <span onClick={() => toggleDescription(index)}>
+                    {song.author} - {song.name}
+                  </span>
                   {song.showDescription && (
-                  <ul>
-                    <li>{song.description}</li>
-                  </ul>
+                    <ul>
+                      <li>{song.description}</li>
+                    </ul>
                   )}
                 </li>
               )}
